b9933629c0890476f7962b9c7b6ff823c063a796
Chris Sievering <chris.sievering@planetpayment.com>
2022-07-22 09:22:50 -0400

    storage account test

--- stacks/staging/cloud-runs/app2/_terramate_generated_backend.tf
+++ stacks/staging/cloud-runs/app2/_terramate_generated_backend.tf
@@ -6,7 +6,6 @@ terraform {
     container_name       = "tfstate"
     key                  = "terramate_staging_app2"
     resource_group_name  = "rg_chris_sandbox"
-    sas_token            = "sp=racwl&st=2022-07-21T18:56:36Z&se=2022-07-22T02:56:36Z&spr=https&sv=2021-06-08&sr=c&sig=ihCie5UE7jIKorBalgtHD2GYOUGJR%2F1g3kImB7e%2By6s%3D"
     storage_account_name = "sacstfstate"
   }
 }
